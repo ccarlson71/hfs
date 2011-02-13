062312b9c356d92f7a93efcd0771c466ed1f7f63
Chris Carlson <chris@intermittence.net>
2011-02-12 19:07:34 -0600

    Added initial player seed handler

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
+PlayerClass.delete_all
 player_classes = PlayerClass.create([
   { :name => 'Archer' },
   { :name => 'Assassin' },
@@ -22,6 +23,25 @@ player_classes = PlayerClass.create([
   { :name => 'Samurai' }
   ])
   
-players = Player.create([
-  { :name => 'Chris Carlson', :dues_paid => true, :dues_paid_through => Date.civil(2037,12,31) }
-  ])
+Player.delete_all
+CreditEntry.delete_all
+
+open("db/seed_data/player_list.txt") do |players|
+  players.read.each_line do |player|
+    next if player[0..0] == "#"
+    player_name,class_list = player.chomp.split("|")
+    puts "New player: #{player_name}"
+    new_player = Player.create!(:name => player_name, :dues_paid => false, :dues_paid_through => nil)
+    if class_list != nil
+      class_list.split(",").each do |class_entry|
+        class_name,credits = class_entry.split(":")
+        puts "  -- #{class_name}: #{credits}"
+        new_credit_entry = CreditEntry.create!(
+          :player_class_id => PlayerClass.find_by_name(class_name).id,
+          :total_credits => credits,
+          :initial_credits => credits)
+        new_player.credit_entries << new_credit_entry
+      end
+    end
+  end
+end
